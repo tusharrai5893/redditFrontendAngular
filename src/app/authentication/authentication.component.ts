d058345a1b61c8cfe34e256f5927cf69e96fd475
Tushar Rai <tushar@Tushars-MacBook-Pro.local>
2021-10-22 02:24:07 +0530

    added signUp page

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -6,17 +6,30 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./authentication.component.css'],
 })
 export class AuthenticationComponent implements OnInit {
-  
-  constructor() {
-    
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 
-  
-  onSignIn(event:Event): void {
+  onSignIn(event: Event): void {
     event.preventDefault();
     console.log(event);
-    
+  }
+
+  onCreateAccount(): void {
+    const createAccountBtn = document.getElementsByClassName('auth');
+
+    const formIsHidden = createAccountBtn[0].children;
+
+    if (formIsHidden[0].id == 'signIn') {
+      console.log('in signin');
+
+      formIsHidden[1].classList.remove('form-hidden');
+      formIsHidden[0].classList.add('form-hidden');
+      formIsHidden[0].remove();
+    } else if (formIsHidden[1].id == 'signUp') {
+      console.log('in signUP');
+      formIsHidden[0].classList.remove('form-hidden');
+      formIsHidden[1].classList.add('form-hidden');
+    }
   }
 }
